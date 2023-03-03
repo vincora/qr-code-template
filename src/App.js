b0487ac61c4a270788d5410a8d1c2026fcbd42ad
julia <julichis@mail.ru>
2023-03-03 18:32:21 +0400

    Fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div className="body">
         <img src={qr} alt="" className="body__pic"/>
         <div className="body__text">
-          <div className="body__title">Improve your front-end skills by building projects</div>
+          <div className="body__title">iyiyyiyiy Improve your front-end skills by building projects</div>
           <div className="body__description">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</div>
         </div>
       </div>
